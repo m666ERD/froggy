2d9c2a9bd444a0d764c0e873469e376fda08443a
girvel <widauka@ya.ru>
2024-01-24 22:51:43 +0500

    Stylistic changes

--- main.lua
+++ main.lua
@@ -1,21 +1,24 @@
 y = 300
 x = 400
+speed = 30
+
 function love.draw()
   love.graphics.setColor(0, 1, 0, 0.7)
   love.graphics.setBackgroundColor(0.02, 0.1, 0.03)
   love.graphics.print("@", x, y)
 end
+
 function love.keypressed(key)
   if key == 's' then 
-    y = y+30
+    y = y + speed
   end
   if key == 'w' then
-    y = y-30
+    y = y - speed
   end
   if key == 'a' then
-    x = x-30
+    x = x - speed
   end
   if key == 'd' then
-    x = x+30
+    x = x + speed
   end
 end    
