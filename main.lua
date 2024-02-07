49523d5ae2dc0923871903573c9c784f258dd97a
BuildTools <unconfigured@null.spigotmc.org>
2024-02-07 20:31:49 +0500

    Add smooth movement

--- main.lua
+++ main.lua
@@ -1,27 +1,28 @@
 y = 300
 x = 400
-speed = 30
-
+speed = 50
 function love.load()
   froggy = love.graphics.newImage("sprites/Froggy's face.png")
 end
 
 function love.draw()
-  love.graphics.draw(froggy, x, y)
+  love.graphics.draw(froggy, math.floor(x), math.floor(y))
   love.graphics.setBackgroundColor(0.02, 0.1, 0.03)
 end
 
-function love.keypressed(key)
-  if key == 's' then 
-    y = y + speed
+function love.update(dt)
+
+  if love.keyboard.isDown ("s") then
+    y = y + speed * dt
   end
-  if key == 'w' then
-    y = y - speed
+  if love.keyboard.isDown ("w") then
+    y = y - speed * dt
   end
-  if key == 'a' then
-    x = x - speed
+  if love.keyboard.isDown ("a") then
+    x = x - speed * dt
   end
-  if key == 'd' then
-    x = x + speed
+  if love.keyboard.isDown ("d") then
+    x = x + speed * dt
   end
 end    
+print(1)
