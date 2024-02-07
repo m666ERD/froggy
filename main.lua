cb9b5569fb17e398bbf32302a70d118950420d2d
BuildTools <unconfigured@null.spigotmc.org>
2024-02-07 21:06:12 +0500

    Introduce objects

--- main.lua
+++ main.lua
@@ -1,28 +1,36 @@
-y = 300
-x = 400
-speed = 50
 function love.load()
-  froggy = love.graphics.newImage("sprites/Froggy's face.png")
+  froggy = {
+    x = 400,
+    y = 300,
+    speed = 70,
+    sprite = love.graphics.newImage("sprites/Froggy's face.png"),
+  }
+
+  grass = {
+    x = 200,
+    y = 300,
+    sprite = love.graphics.newImage("sprites/grass.png"),
+  }
 end
 
 function love.draw()
-  love.graphics.draw(froggy, math.floor(x), math.floor(y))
+  love.graphics.draw(froggy.sprite, math.floor(froggy.x), math.floor(froggy.y))
+  love.graphics.draw(grass.sprite, grass.x, grass.y)
   love.graphics.setBackgroundColor(0.02, 0.1, 0.03)
 end
 
 function love.update(dt)
-
   if love.keyboard.isDown ("s") then
-    y = y + speed * dt
+    froggy.y = froggy.y + froggy.speed * dt
   end
   if love.keyboard.isDown ("w") then
-    y = y - speed * dt
+    froggy.y = froggy.y - froggy.speed * dt
   end
   if love.keyboard.isDown ("a") then
-    x = x - speed * dt
+    froggy.x = froggy.x - froggy.speed * dt
   end
   if love.keyboard.isDown ("d") then
-    x = x + speed * dt
+    froggy.x = froggy.x + froggy.speed * dt
   end
 end    
 print(1)
